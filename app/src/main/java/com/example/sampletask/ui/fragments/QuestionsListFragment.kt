cd8c3b2e5531f3bfd60f30c51d937e7051f8882f
YashKr01 <yashkumar201301@gmail.com>
2021-12-08 02:02:05 +0530

    Loading data from local JSON file

--- app/src/main/java/com/example/sampletask/ui/fragments/QuestionsListFragment.kt
+++ app/src/main/java/com/example/sampletask/ui/fragments/QuestionsListFragment.kt
@@ -1,23 +1,26 @@
 package com.example.sampletask.ui.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.sampletask.R
 import com.example.sampletask.adapters.QuestionsAdapter
 import com.example.sampletask.databinding.FragmentQuestionsListBinding
 import com.example.sampletask.model.QuestionResponse
-import com.example.sampletask.utils.Resource
 import com.example.sampletask.viewmodel.QuestionsViewModel
-import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.IOException
+import java.nio.charset.Charset
 
 @AndroidEntryPoint
 class QuestionsListFragment : Fragment() {
@@ -36,22 +39,24 @@ class QuestionsListFragment : Fragment() {
         _binding = FragmentQuestionsListBinding.inflate(inflater, container, false)
 
         // setting up recycler view and adapter
-        var list = emptyList<QuestionResponse>()
+        val questionsList = arrayListOf<QuestionResponse>()
         val questionAdapter = QuestionsAdapter(requireContext(),
             onItemClick = { position ->
-                val listArgument = arrayListOf<QuestionResponse>()
-                if (binding.txtFilter.text.equals(resources.getString(R.string.not_attempted))) {
-                    listArgument.clear()
-                    listArgument.addAll(list)
-                } else {
-                    listArgument.clear()
-                    listArgument.addAll(viewModel.databaseList)
-                }
-                val action =
+
+                val action1 =
                     QuestionsListFragmentDirections.actionQuestionsListFragmentToQuestionDetailFragment(
-                        listArgument.toTypedArray(), position
+                        questionsList.toTypedArray(), position
                     )
-                findNavController().navigate(action)
+
+                val action2 =
+                    QuestionsListFragmentDirections.actionQuestionsListFragmentToQuestionDetailFragment(
+                        viewModel.databaseList.toTypedArray(), position
+                    )
+
+                if (binding.txtFilter.text.equals(resources.getString(R.string.not_attempted)))
+                    findNavController().navigate(action1)
+                else findNavController().navigate(action2)
+
             })
 
         binding.recyclerViewQuestions.apply {
@@ -59,42 +64,6 @@ class QuestionsListFragment : Fragment() {
             layoutManager = LinearLayoutManager(requireContext())
             setHasFixedSize(true)
             adapter = questionAdapter
-        }
-
-        // loading from viewModel
-        if (!viewModel.isLoaded) viewModel.getQuestionsList()
-
-        // observe viewModel data
-        viewModel.list.observe(viewLifecycleOwner) { result ->
-//            binding.layoutEmpty.visibility = View.GONE
-            when (result) {
-                is Resource.Loading -> {
-                    binding.shimmerLayout.startShimmer()
-                }
-                is Resource.Success -> {
-                    binding.shimmerLayout.apply {
-                        stopShimmer()
-                        visibility = View.GONE
-                    }
-                    list = result.data!!
-                    questionAdapter.submitList(result.data)
-                }
-                is Resource.Error -> {
-                    binding.shimmerLayout.apply {
-                        stopShimmer()
-                        visibility = View.GONE
-                    }
-                    Snackbar.make(
-                        requireContext(),
-                        binding.root,
-                        "An Error Occurred",
-                        Snackbar.LENGTH_SHORT
-                    ).setAction("RETRY") {
-                        binding.shimmerLayout.startShimmer()
-                        viewModel.getQuestionsList()
-                    }.show()
-                }
-            }
         }
 
         viewModel.getAttemptedQuestionsList().observe(viewLifecycleOwner) { questionList ->
@@ -113,7 +82,45 @@ class QuestionsListFragment : Fragment() {
             attempted = !attempted
         }
 
+        lifecycleScope.launch {
+            delay(3000)
+            questionsList.addAll(getData())
+            viewModel.storedList = questionsList
+            questionAdapter.submitList(questionsList)
+            binding.shimmerLayout.apply {
+                stopShimmer()
+                visibility = View.GONE
+            }
+        }
+
         return binding.root
+    }
+
+    private fun getData(): List<QuestionResponse> {
+        return try {
+            val type = object : TypeToken<List<QuestionResponse>>() {}.type
+            Gson().fromJson(loadJSONFromAsset(), type)
+
+        } catch (e: IOException) {
+            emptyList()
+        }
+    }
+
+    private fun loadJSONFromAsset(): String {
+        val json: String?
+        try {
+            val inputStream = resources.assets.open("questions.json")
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            val charset: Charset = Charsets.UTF_8
+            inputStream.read(buffer)
+            inputStream.close()
+            json = String(buffer, charset)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return ""
+        }
+        return json
     }
 
     override fun onDestroyView() {
